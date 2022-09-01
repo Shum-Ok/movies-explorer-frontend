c30ed4180539e0610b4b40f7c303fc8ab28dbf6e
ShumOk <shum_ok@bk.ru>
2022-09-01 23:18:36 +0300

    fix: Страница «404». Кнопка «Назад>, возвращает на предыдущую страницу

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,16 +1,22 @@
 // react
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 // css
 import './PageNotFound.css';
 
 function PageNotFound() {
+  const history = useHistory()
+
+  function goToBack() {
+    history.goBack()
+  }
+
   return (
     <section className='not-found'>
       <div className='not-found__content'>
         <h1 className='not-found__title'>404</h1>
         <p className='not-found__text'>Страница не найдена</p>
       </div>
-      <Link to='/' className='not-found__link'>Назад</Link>
+      <button onClick={goToBack} className='not-found__link'>Назад</button>
     </section>
   );
 };
